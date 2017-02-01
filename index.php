75fa74667071ed867c78427c85c69e4c70f31798
Oswaldo Teran <oswaldo_217@hotmail.com>
2017-02-01 15:53:48 -0400

    subiendo cambios para merge de prueba

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="web progress">
-    <title>CheyPoducion</title>
+    <meta name="author" content="CheyPoducciones">
+    <title>CheyPoducciones</title>
     <link href="static/css/bootstrap.min.css" rel="stylesheet">
     <link href="static/css/font-awesome.min.css" rel="stylesheet">
     <link href="static/css/animate.min.css" rel="stylesheet">
