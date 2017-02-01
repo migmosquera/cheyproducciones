b6643d0e89993e2767e3c0e33a7d07c997be9999
Oswaldo Teran <oswaldo_217@hotmail.com>
2017-02-01 15:56:53 -0400

    subiendo cambios para merge de prueba

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="CheyPoducciones">
+    <meta name="CheyPoducciones" content="pagina de musica venezolana">
+    <meta name="robots" content="musica, barquisimeto, venezuela">
     <title>CheyPoducciones</title>
     <link href="static/css/bootstrap.min.css" rel="stylesheet">
     <link href="static/css/font-awesome.min.css" rel="stylesheet">
@@ -17,11 +17,11 @@
     <link href="static/css/main.css" rel="stylesheet">
     <link href="static/css/responsive.css" rel="stylesheet">
   
-    <link rel="shortcut icon" href="static/images/ico/favicon.ico">
+    <!--<link rel="shortcut icon" href="static/images/ico/favicon.ico">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="static/images/ico/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="static/images/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="static/images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="static/images/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="static/images/ico/apple-touch-icon-57-precomposed.png">-->
 </head>
 
 <body id="home" class="homepage">
